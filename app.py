84f3b9511535bdc5c94fe9ec17b073c5b4439700
runpatrick <34929307+runpatrick@users.noreply.github.com>
2022-11-15 04:11:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def execute():
     options.add_argument("--headless")
     options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36')
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH , chrome_options=options)
-    driver.get("https://quizizz.com/join/pre-game/running/U2FsdGVkX1%252FA05gJRrhUEvnulHf3SbRL2%252FGm0QQRtm3maa6FUM%252BXF3qoxdhjjRoJlWwrB11CW781PR5kDSl2nQ%253D%253D/start")
+    driver.get("https://quizizz.com/join?gc="+quizid+"&source=liveDashboard")
     time.sleep(1)
     driver.find_element_by_xpath("//div[@id='root']/div/div/div/div/div/div[2]/div[2]/div/div/form/div/div[2]/div[2]/i").click()
     time.sleep(1)
